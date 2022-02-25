720b62c0707bf04067598fb8a37c38f2c3429da7
Kirill Markov <victory322@yandex.ru>
2022-02-25 15:59:47 +0300

    reset

--- changes.js
+++ changes.js
@@ -1,6 +1,19 @@
+<<<<<<< HEAD
 const copyObjectA = objectA ;
 copyObjectA.a = 50 ;
 copyObjectA.c = 'xyz';
 console.log(objectA);
 
 alert(objectA);
+=======
+const objectA = {
+  a: 10,
+  b: "abc",
+};
+
+const copyObjectA = { ...objectA };
+copyObjectA.a = 50;
+copyObjectA.c = "xyz";
+console.log(objectA);
+alert(objectA);
+>>>>>>> main
